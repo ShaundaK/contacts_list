035ac92fc0a560afffb15c94d8e66a05a501aeb3
ShaundaK <sdking1913@gmail.com>
2018-11-05 22:52:45 -0600

    html show, create and new action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,26 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
     render "index.html.erb"
   end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def create
+     @contact = Contact.new(
+      first_name: params[:first_name],
+      middle_name: params[:middle_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      user_id: 1
+      )
+    @contact.save
+    redirect_to "/contacts"
+     end
+
+  def new
+    render "new.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "contacts" => "contacts#index"
+  get "contacts/new" => "contacts#new"
+  post "contacts" => "contacts#create"
+  get "contacts/:id" => "contacts#show"
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
